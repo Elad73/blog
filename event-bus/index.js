2f100d63181c8951cc46cb61cbb5153274499ee5
EladQM <elad.ron@qmarkets.net>
2023-02-18 05:58:16 +0200

    adding services communicatino on event-bus"

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,9 +17,9 @@ app.post('/events', (req, res) => {
     events.push(event); // storing the upcoming event in the data store
 
     axios.post('http://posts-clusterip-service:4000/events', event);
-    // axios.post('http://localhost:4001/events', event);
-    // axios.post('http://localhost:4002/events', event);
-    // axios.post('http://localhost:4003/events', event);
+    axios.post('http://comments-service:4001/events', event);
+    axios.post('http://query-service:4002/events', event);
+    axios.post('http://moderation-service:4003/events', event);
 
     res.send({ status: 'OK' });
 });
