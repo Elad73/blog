f7d4e4fb31dfdc826821e3240baf4844fab93e9e
Elad73 <elad.ron.g@gmail.com>
2023-02-19 17:10:27 +0200

    changing create post api

--- posts/index.js
+++ posts/index.js
@@ -21,7 +21,7 @@ app.get('/posts', (req, res) => {
 /** Creating new post
  * 
  */
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex'); 
     const { title } = req.body;
 
