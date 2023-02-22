e2631519be54c0b0b0e50c7b96b6209d40a02ae7
Elad73 <elad.ron.g@gmail.com>
2023-02-22 22:51:53 +0200

    Determining Error Type

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { ReqeustValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
     err: Error,
@@ -6,7 +8,13 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => { 
-    console.log('Something went wrong', err);
+    if (err instanceof ReqeustValidationError) {
+        console.log('handling this error as a request validation error');
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log('handling this error as a db connection error');
+    }
 
     res.status(400).send({
         message: err.message
