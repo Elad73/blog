c99ed50b3fb882167884bd1e65856c74c0b288cc
Elad73 <elad.ron.g@gmail.com>
2023-02-23 10:45:14 +0200

    Adding static Properties to a Model

---
+++ ticketing/auth/src/model/user.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties
+// that are required to create a new user
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// An interface that describes the properties that a User Model has
+interface UserModel extends mongoose.Model<any>{
+    build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// method to ensure the attributes needed for a user creation - verifyied by TypeScript
+userSchema.statics.build = (attrs: UserAttrs) => { 
+    return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
