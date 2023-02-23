9d12ffbd6b69d2257b5183d3a595e9600a4f740b
Elad73 <elad.ron.g@gmail.com>
2023-02-23 12:26:25 +0200

    Mongoose Pre-Save Hooks

--- ticketing/auth/src/model/user.ts
+++ ticketing/auth/src/model/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../utils/password";
 
 // An interface that describes the properties
 // that are required to create a new user
@@ -28,6 +29,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    } 
+    done();
 });
 
 // method to ensure the attributes needed for a user creation - verifyied by TypeScript
