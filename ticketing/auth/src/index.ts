8a096bb78792baae976cf3c93b89713454cda1e8
Elad73 <elad.ron.g@gmail.com>
2023-02-23 02:11:07 +0200

    Connecting to mongoDB

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/siginin';
@@ -23,7 +24,18 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+        console.log("Connected to mongoDB");
+    } catch (err) {
+        console.error(err);
+    }
 
-app.listen(3000, () => {
-    console.log("Listeniong on port 3000!!!!!!!!!!");
-});
+    app.listen(3000, () => {
+        console.log("Listeniong on port 3000!!!!!!!!!!");
+    });
+};
+
+start();
+
