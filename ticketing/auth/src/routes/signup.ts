3cfd5a7cb273aa77173eaf95938584c309245914
Elad73 <elad.ron.g@gmail.com>
2023-02-24 07:06:53 +0200

    Generating a JWT

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../model/user';
 import { ReqeustValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -32,6 +33,17 @@ router.post('/api/users/signup',
 
         const user = User.build({ email, password });
         await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'asdf');
+
+        // store it on session object.
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(user);
     }
